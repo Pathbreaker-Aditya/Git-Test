8756771a41334a2ef81f0943e17f67888031b4b1
Aditya <aditya111597@gmail.com>
2023-10-25 16:15:00 +0530

    Attempting Git push

---
+++ helloGit.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"\n  Name : Aditya Joshi\n  Roll No. - 523410001";
+    cout<<"\n  Roll No : 523410001";
+    cout<<"\n  Group : 1st";
+    return 0;
+}
